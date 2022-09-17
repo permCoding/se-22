222c8e04e09c5f6e5b267ef09a9a52f22b48e394
permCoding <ttxiom@gmail.com>
2022-09-17 08:57:42 +0500

    iterator

--- part-1/themes/01-array/01-array-methods/06.js
+++ part-1/themes/01-array/01-array-methods/06.js
@@ -1,25 +1,37 @@
 // some every keys includes slice splice indexOf findIndex
 
-let arr = [0, 2, 4, 6, 8, 9];
-
-console.log('some =', arr.some(x => x%2 != 0));
-console.log('every =', arr.every(x => x%2 != 0));
-
-console.log(arr.keys()); // итератор
-// бывают ещё генераторы - это в теме theme-03-func
-
-for (let key of arr.keys()) {
-    console.log(`key = ${key}, value = ${arr[key]}`);
+let ex1 = (arr) => {
+    console.log('some =', arr.some(x => x%2 != 0));
+    console.log('every =', arr.every(x => x%2 != 0));
 }
 
-console.log('- '.repeat(9));
+let ex2 = (arr) => {
+    console.log(arr.keys()); // итератор    
+    // бывают ещё генераторы - это в теме theme-03-func
+    console.log(...arr.keys()); // элементы итератора
+    console.log(...arr.values()); // элементы итератора
 
-console.log(arr.includes(4));
-console.log(arr.includes(5));
-console.log(arr.indexOf(4));
-console.log(arr.findIndex(x => (x%3 == 0) && (x > 0)));
+    for (let key of arr.keys()) {
+        console.log(`key = ${key}, value = ${arr[key]}`);
+    }
+    let iterator = arr.entries();
+    for (let [key, value] of iterator) {
+        console.log(`key = ${key}, value = ${value}`);
+    }  
+}
 
-console.log(arr.slice(1,3)); // не включая правую границу
-console.log(arr);
-console.log(arr.splice(1,3)); // включительно
-console.log(arr);
+let ex3 = (arr) => {
+    console.log(arr.includes(4));
+    console.log(arr.includes(5));
+    console.log(arr.indexOf(4));
+    console.log(arr.findIndex(x => (x%3 == 0) && (x > 0)));
+
+    console.log(arr.slice(1,3)); // не включая правую границу
+    console.log(arr);
+    console.log(arr.splice(1,3)); // включительно
+    console.log(arr);
+}
+
+
+let arr = [0, 2, 4, 6, 8, 9];
+ex2(arr);
