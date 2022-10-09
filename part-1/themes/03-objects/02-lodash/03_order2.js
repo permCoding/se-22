ef268fcb4a7828eadc63de2339c023116f71a0db
Андрей Беляков <ttxiom@gmail.com>
2022-10-09 12:10:14 +0500

    up

--- part-1/themes/03-objects/02-lodash/03_order2.js
+++ part-1/themes/03-objects/02-lodash/03_order2.js
@@ -19,7 +19,7 @@ const users = [
  * нативный метод - мутабельный
  * lodash - не мутабельный
  */
-function _sort1() {
+function _sortBy_1() {
     _
         .sortBy(users, obj => obj.name) // чистая функция
         .forEach(obj => console.log(obj));
@@ -32,29 +32,42 @@ function _sort1() {
 }
 
 /**
- * lodash - по двум параметрам  
+ * lodash - по двум параметрам - sortBy  
  * обычный подход
  */
-function _sort2() {
-    console.log(_.sortBy(_.sortBy(users, obj => obj.name), obj => obj.age));
-    console.log(_.sortBy(_.sortBy(users, ['age']), ['name']));
-    console.log(_.sortBy(_.reverse(_.sortBy(users, obj => obj.age)), obj => obj.name)); // для обратного порядка нужно повозиться
+function _sortBy_2() {
+    let f_sort = (a,b) => {
+        if (a.name > b.name) return 1;
+        if (a.name < b.name) return -1;
+        return b.age - a.age;
+    }
+
+    // console.log(users.sort(f_sort)); // нативный способ js - он испортит исходный массив
+
+    // console.log(_.sortBy(_.sortBy(users, obj => obj.name), obj => obj.age)); // не мутабельный
+    // console.log(_.sortBy(_.sortBy(users, ['age']), ['name'])); // не мутабельный
+    console.log(_.sortBy( // для обратного порядка нужно повозиться
+        _.reverse(_.sortBy(users, obj => obj.age)), obj => obj.name)
+    );
 }
 
 /**
- * lodash - по двум параметрам  
+ * lodash - по двум параметрам - orderBy  
  * подход с параметрами сортировки  
  * и направлениями asc, desc  
  * в двух нотациях  
  */
-function _sort3() {
+function _orderBy_2() {
     console.log(_.orderBy(users, ['name','age'], ['asc','desc']));
     console.log(_(users).orderBy(['name','age'], ['asc','desc']).value());
 }
 
 
-// _sort1();
-// _sort2();
-_sort3();
+console.log('- '.repeat(9));
 
+// _sortBy_1();
+// _sortBy_2();
+_orderBy_2();
+
+console.log('проверка на мутабельность:')
 console.table(users); // проверка сохранения массива
