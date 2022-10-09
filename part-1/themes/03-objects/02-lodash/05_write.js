8492c0ddcb1ec39acf3248f03fa7e775b930abbc
Андрей Беляков <ttxiom@gmail.com>
2022-10-10 04:11:23 +0500

    up

--- part-1/themes/03-objects/02-lodash/05_write.js
+++ part-1/themes/03-objects/02-lodash/05_write.js
@@ -7,6 +7,7 @@ function write_1(mens) {
             './json/data_check.json', 
             JSON.stringify(_.orderBy(mens, ['name','age'], ['asc','desc']), null, 2)
         );
+        console.log('- file saved');
 }
 
 function write_2(mens, new_items) {
@@ -17,15 +18,26 @@ function write_2(mens, new_items) {
             './json/data_check.json', 
             JSON.stringify(_(mens).orderBy(['name','age'], ['asc','desc']).value(), null, 4)
         );
+    console.log('- file saved');
 }
+
+console.clear();
 
 let objs = [
     { "name": "Яна", "age": 25 },
     { "name": "Яна", "age": 55 },
     { "name": "Алекс", "age": 21 }
 ]
-let mens = require('./json/mens.json');
+let mens = null;
+try {
+    mens = require('./json/mens.json'); // проверить изменив имя файла    
+} catch (error) {
+    console.error(error.message);
+    mens = undefined;
+}
+finally {
+    console.log('-\n'.repeat(5));
+}
 
-console.clear();
-// write_1(mens);
-write_2(mens, objs);
+write_1(mens);
+// write_2(mens, objs);
