40afb659decebce79ae91c2d2d062b86dd6f1cbb
Андрей Беляков <ttxiom@gmail.com>
2022-12-05 21:55:46 +0500

    course project

--- part-1/themes/05-rest/module.js
+++ part-1/themes/05-rest/module.js
@@ -6,16 +6,18 @@ const about = (res) => {
 const get_files_filter = (dir, ext) => {
     const rd = require('fs').readdirSync;
     const st = require('fs').statSync;
+    const path = require('path');
     const rec = (dir) => {
         let items = rd(dir, 'utf-8');
         for (let item of items) {
-            let path = dir + item + '/';
-            if (st(path).isDirectory()) { // шаг рекурсии - директория
-                rec(path, ext);
+            let path_item = path.join(dir, item.toString());
+            if (st(path_item).isDirectory()) {
+                rec(path_item, ext); // шаг рекурсии - если директория
             }
             else { // точка останова - файл
-                // сначала проверить ext
-                name_files.push(item);
+                // фильтр по выбранному типу файлов
+                // добавлять в массив если правильное ext
+                name_files.push(path_item);
             }
         }
     }
