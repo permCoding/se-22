a12a4b0aa4b38f939aeb994a13752746907361f8
Андрей Беляков <ttxiom@gmail.com>
2023-03-16 14:53:15 +0500

    up

--- part-2/labrabs/labrab-03-select/app/controllers/controller_post.js
+++ part-2/labrabs/labrab-03-select/app/controllers/controller_post.js
@@ -1,12 +1,11 @@
 const render_data = (req, res) => {
-    let d = req.body.direct;
-    if (d < 0) d = 0;
-    // console.log('- ', d); // просто для контроля
-    
     // 1 - получаем данные
     let { model } = require('../models/model.js');
 
     // 2 - далее настраиваем данные
+    let d = req.body.direct;
+    if (d < 0) d = 0;
+    // console.log('- ', d); // просто для контроля
     let field = d<2? 'id': 'title'; // поле для сортировки
     model.field = field;
     let direct = d%2==0? +1: -1; // направление для сортировки
